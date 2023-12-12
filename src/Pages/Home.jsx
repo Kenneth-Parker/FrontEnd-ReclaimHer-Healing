36b47f15715d82a7e514a6790040a8a10c4030aa
Kenneth-Parker <kennethkavon@gmail.com>
2023-12-12 16:52:10 -0500

    home img add button bckgrnd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './Home.css'
+import img1 from "../assets/grpWoman.jpeg"
+import img2 from "../assets/groWomanHug.png"
+
+
 
 const Home = () => {
     return (
         <div>
-            Welcome To "ReclaimHer Healing"
+            <section className="welcome-section">
+                <div className="welcome-container">
+                    <img
+                        src={img1}
+                        alt="homePage1 Image"
+                        className="homePage-image"
+                    />
+
+                    <h1>Welcome to ReclaimHer Healing</h1>
+                    <p>Empowering women on the journey to healing and transformation.</p>
+                    <img
+                        src={img2}
+                        alt="homePage2 Image"
+                        className="homePage-image"
+                    />
+                    <br />
+                    <button>Join Us</button>
+                </div>
+            </section>
         </div>
     );
 };
