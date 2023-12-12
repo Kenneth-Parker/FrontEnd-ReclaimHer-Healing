9ba9e73d0f2059ee35025dc5fe670bf97244fa99
Kenneth Parker <131785555+Kenneth-Parker@users.noreply.github.com>
2023-12-11 23:04:26 -0500

    Update NavBar.jsx menu button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './NavBar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav>
             <Link to={"/"}><h1>ReclaimHer Healing</h1></Link>
-            <ul>
+            <div className={`menu-btn ${menuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
+                <div className="btn-line"></div>
+                <div className="btn-line"></div>
+                <div className="btn-line"></div>
+            </div>
+            <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
                 <li><Link to={"/"}>Home</Link></li>
                 <li><Link to={"/about"}>About</Link></li>
                 <li><Link to={"/forum"}>Forum</Link></li>
